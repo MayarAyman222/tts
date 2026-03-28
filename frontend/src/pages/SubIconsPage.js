bfe5eef619d4dc49b96ca42cc7b36663c842d66d
MayarAyman222 <mayarayman222@gmail.com>
2026-03-28 15:44:42 +0200

    uppdates

--- frontend/src/pages/SubIconsPage.js
+++ frontend/src/pages/SubIconsPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Card, Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
